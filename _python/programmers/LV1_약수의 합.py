851278c4780017dfe85bcec5d8f36fd324f930a8
kimseunggyu <dev.seunggyu@gmail.com>
2019-01-30 10:00:00 +0900

    programmers LV1_약수의 합

---
+++ _python/programmers/LV1_약수의 합.py
@@ -0,0 +1,15 @@
+#
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            answer += i
+
+    return answer
+
+"""
+[참고]
+
+def solution(num):
+    return num + sum([i for i in range(1, (num // 2) + 1) if num % i == 0])
+"""
