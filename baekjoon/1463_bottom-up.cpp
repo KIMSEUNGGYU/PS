d6f82fffc6ac2b3ef21b22cb2702b16361bc94a2
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2019-06-12 17:33:13 +0900

    baekjoon 1463 - 1로 만들기

--- baekjoon/1463_bottom-up.cpp
+++ baekjoon/1463_bottom-up.cpp
@@ -18,10 +18,10 @@ int main() {
 		dp[i] = dp[i-1]+1;
 
 		// "/2" 연산
-		if(i%2==0 && dp[i]>dp[n/2]+1) dp[i] = dp[n/2]+1;
+		if(i%2==0 && dp[i]>dp[i/2]+1) dp[i] = dp[i/2]+1;
 
 		// "/3" 연산
-		if(i%3==0 && dp[i]>dp[n/3]+1) dp[i] = dp[n/3]+1;
+		if(i%3==0 && dp[i]>dp[i/3]+1) dp[i] = dp[i/3]+1;
 	}
 
 	cout << dp[n] << "\n";
