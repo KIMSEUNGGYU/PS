4ebde9bde98e2541aeccc51f1ec9ff44b2433320
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2019-06-12 17:50:03 +0900

    baekjoon 11726 - 2xn 타일링

--- baekjoon/11726.cpp
+++ baekjoon/11726.cpp
@@ -1,36 +1,30 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-const int MAX = 1001;
-int d[MAX];	// d[n]: n크기의 직사각형을 채우는 방법의 수
 
-// Top-down 방식
-int go(int n) {
-	// 예외 처리?
-	if(n <= 1) return 1;
+// 문제
+int dp[1001]; 	// 2*n 직사각형을 채우는 타일의 수
 
-	// memoization
-	if(d[n] > 0) return d[n];
+// 작은 문제
+// dp[n-1];	// 2*1 타일
+// dp[n-2]; // 1*2 타일
 
-	// 값 쓰기
-	d[n] = (go(n-1)+go(n-2)) %10007;
-	
-	return d[n];
-}
-
+// 식 정의
+// dp[i] = dp[i-2]+dp[i-1]
 
 int main() {
 	int n;
-	scanf("%d", &n);
+	cin >> n;
 
-	d[0] = 1;
-	d[1] = 1;
-	// Bottom-up
+	// 예외 처리?
+	dp[0] = 1;
+	dp[1] = 1;
+
+	// Bottom-up 방식
 	for(int i=2; i<=n; i++) {
-		d[i] = d[i-1] + d[i-2];
-		d[i] %= 10007;
+		dp[i] = (dp[i-2]+dp[i-1]) % 10007;
 	}
 
-	printf("%d\n", d[n]);
+	cout << dp[n] << "\n";
 
 	return 0;
 }
