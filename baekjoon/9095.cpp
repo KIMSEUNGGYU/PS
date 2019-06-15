ee31aabde12c98fd28b279666386983024ff44a5
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2019-06-15 10:49:27 +0900

    baekjoon 9095 - 1, 2, 3 더하기

--- baekjoon/9095.cpp
+++ baekjoon/9095.cpp
@@ -1,24 +1,33 @@
-#include <iostream>
+/* 
+- dp 정의
+  dp[11]: n을 1, 2, 3의 합으로 만들 수 있는 방법의 수
+
+- 식 정의
+  dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
-const int MAX = 11;
-int d[MAX];	// n을 나타내는 방법의 수
 
+int dp[11];		// n을 1, 2, 3의 합으로 만들 수 있는 방법의 수
 int main() {
-	d[0] = 1;
-	for(int i=1; i<=10; i++) {
-		if(i-1 >= 0) d[i] += d[i-1];
-		if(i-2 >= 0) d[i] += d[i-2];
-		if(i-3 >= 0) d[i] += d[i-3];
-	}
+	// dp 구하기
+	dp[0] = 1;
+	dp[1] = 1;
+	dp[2] = 2;
 
-	int t; 
-	scanf("%d", &t);
-
-	while(t--) {
-		int n;
-		scanf("%d", &n);
-		printf("%d\n", d[n]);
+	for(int i=3; i<=11; i++) {
+		dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
 	}
 	
+	// 값 입력 및 출력
+	int n;
+	cin >> n;
+	while(n--) {	
+		int x;
+		cin >> x;
+		cout << dp[x] <<"\n";
+	}
+
 	return 0;
 }
