cfaa02f99480550535b837a7128ea11d09b4c70e
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2019-06-18 10:59:58 +0900

    baekjoon 11053 - 가장 긴 증가하는 부분 수열

--- baekjoon/11053.cpp
+++ baekjoon/11053.cpp
@@ -1,37 +1,39 @@
-// LIS (최장증가수열), DP
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+int a[1001];	// 수열의 값
+int dp[1001];	// i번째 수열에서 가장 긴 부분 수열 길이
 int main() {
-	ios_base::sync_with_stdio(0);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int A[1010] = {0, };	// 인덱스 값
-	int d[1010] = {0, };	// 증가 수열의 길이
-	
+
 	int n;
 	cin >> n;
+	for(int i=0; i<n; i++) cin >> a[i];
 
-	// 입력
-	for(int i=0; i<n; i++) cin >> A[i];
-	int max = 0;
+	// 가장 긴 부분 수열 구하기
 	for(int i=0; i<n; i++) {
-		d[i] = 1;	// 각 경우당 증가 수열의 길이는 1, 왜냐하면 자기 자신을 포함하니깐!!
-		
-		// i-1 값들과 비교 (0, 1, ... , i-1)
+		dp[i] = 1;	// 모든 수는 자기 자신을 수열로 만들 수있기 때문에 1
+
 		for(int j=0; j<i; j++) {
-			// i값이 크고(수열이 증가하고),
-			// 이전 증가수열의 길이+1(i값이 크다면 증가수열이고 그렇다면 증가된 상태이니깐 +1)이 더 크면 증가수열의길이 업데이트
-			if(A[i] > A[j] && d[i] < d[j]+1) {
-				d[i] = d[j]+1;
+			// 조건 
+			// 1. 숫자가 커야함 a[j] < a[i] -> 20 < 30
+			// 2. dp에는 부분 수열 중 가장 큰 값이 들어있기 때문에 이전 dp값들을 참조하면서 가장 긴 부분 수열 찾기 
+			// +1를 한 이유는 자기 자신도 수이기 때문에 조건1을 만족하면 증가하는 부분 수열이 되기 때문에
+			if(a[j] < a[i] && dp[i] < dp[j]+1) {
+				dp[i] = dp[j]+1;
 			}
 		}
-
-		// 길이가 가장 큰 것을 기억해두는 용도
-		if(max < d[i]) max = d[i];
-		
 	}
 
-	cout << max <<"\n";
+	// dp 중에서 가장 큰 값 구하기 
+	// 해당 값이 가장 긴 부분 수열이기 때문에
+	int max = -1;
+	for(int i=0; i<n; i++) {
+		if(max < dp[i]) max = dp[i];
+	}
+
+	cout << max << "\n";
+
 	return 0;
 }
