15102208d7530b47c03112a0aca0678f2da4e04b
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2019-06-18 10:01:19 +0900

    baekjoon 11052 - 카드 구매하기

--- baekjoon/11052.cpp
+++ baekjoon/11052.cpp
@@ -1,27 +1,32 @@
-#include <iostream>
-#include <algorithm>
+/*
+- dp 정의
+  dp: 카드 i개를 구할 수 있는 최대 가격
+  p : 카드 i개를 구할 수 있는 가격
+- 식 정의	
+  dp[i] = max(dp[i-j]+p[j]);	// 각 경우당 최대값
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
-const int MAX = 1001;
-int d[MAX];		// d[n]: N개를 갖기 위해 지불해야하는 금액의 최대값
-int p[10001];	// p[i]: i개의 카드를 갖기 위해 지불해야하는 금액
 
-// 식
-// d[n] = p[i]+d[n-i];
+int p[10001];	// 카드 i개의 가격
+int dp[1001];	// 카드 i개를 구할 수 있는 최대 가격
+
 int main() {
-	d[0] = 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
 	int n;
-	scanf("%d", &n);
-
-	for(int i=1; i<=n; i++) scanf("%d", &p[i]);
+	cin >> n;
+	for(int i=1; i<=n; i++) cin >> p[i];
 
 	for(int i=1; i<=n; i++) {
 		for(int j=1; j<=i; j++) {
-			// i개 샀을때 최고 금액
-			d[i] = max(d[i], p[j]+d[i-j]);	
-		}	
-
+			if(dp[i] < dp[i-j] + p[j]) dp[i] = dp[i-j] + p[j];
+		}
 	}
-	printf("%d\n", d[n]);
+
+	cout << dp[n] << "\n";
 
 	return 0;
 }
