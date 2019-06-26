7899a40e0cdb2fc94e2d546ec741d2d2e36d8b70
kimseunggyu <dev.seunggyu@gmail.com>
2019-06-26 22:21:31 +0900

    programmers LV 2. 구명보트

---
+++ programmers/LV 2_구명보트.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int size = people.size();
+    vector<int> check(size);
+    int cnt = 0;
+    bool flag = false;
+    
+    sort(people.begin(), people.end());
+    for(int i=0; i<people.size(); i++) {
+        flag = false;
+        
+        if(check[i] == 1) continue;
+        for(int j=size-1; j>i; j--) {
+            if(people[i]+people[j] <= limit && check[i] == 0 && check[j]==0) {
+                cnt++;
+                check[i] = 1;
+                check[j] = 1;
+                flag = true;
+            }
+        }
+        if(flag == false) {
+            cnt++;
+        }
+        
+    }
+    answer = cnt;
+    return answer;
+}
