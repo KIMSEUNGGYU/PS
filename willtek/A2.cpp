fa5b04a03d481462274ca6679f90086349ecd43b
kimseunggyu <dev.seunggyu@gmail.com>
2019-07-05 22:49:50 +0900

    willtek 20190705

--- willtek/A2.cpp
+++ willtek/A2.cpp
@@ -3,7 +3,7 @@
 // 환율 계산 
 unsigned int exchange(unsigned int yen) {
 	double exchange_rate = 10.1;
-	// float 으로 하면 안됨! 
+	// float 으로 하면 안됨! - 반올림에서 문제가 생김 
 	
 	return (unsigned int) (yen * exchange_rate);
 }
