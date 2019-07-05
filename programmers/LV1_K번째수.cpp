faeebcc4e90387bf59f2731652242f1ad2886342
kimseunggyu <dev.seunggyu@gmail.com>
2019-07-05 22:25:53 +0900

    programmers LV 1. K번째수

--- programmers/LV1_K번째수.cpp
+++ programmers/LV1_K번째수.cpp
@@ -5,7 +5,7 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     
     for(int i=0; i<commands.size(); i++) {
-        // 커맨드 정리, from 부타 to 까지 자르고 idx 의 값 반환
+        // 커맨드 정리, from 부터 to 까지 자르고 idx 의 값 반환
         int from = commands[i][0]-1;
         int to = commands[i][1]-1;
         int idx = commands[i][2]-1;
