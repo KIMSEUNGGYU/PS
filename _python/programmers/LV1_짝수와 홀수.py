ee039ceb7c650a06d33835145115b43649735f68
kimseunggyu <dev.seunggyu@gmail.com>
2020-02-16 22:08:08 +0900

    programmers LV1_짝수와 홀수

---
+++ _python/programmers/LV1_짝수와 홀수.py
@@ -0,0 +1,14 @@
+def solution(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+"""
+[참고]
+def solution(num):
+    if num % 2: 
+        return "Odd"
+    else:
+        return "Even"
+"""
