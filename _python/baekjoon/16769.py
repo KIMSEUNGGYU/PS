4907508f6f5e464b5d2e455075feb978a3540798
kimseunggyu <dev.seunggyu@gmail.com>
2020-06-02 15:05:46 +0900

    baekjoon 16769 - Mixing Milk

---
+++ _python/baekjoon/16769.py
@@ -0,0 +1,18 @@
+C, M = list(), list()
+
+for i in range(3):
+    # 용량, 들어있는 양
+    a, b = map(int, input().split())
+    C.append(a)
+    M.append(b)
+
+
+for i in range(100):
+    idx = i % 3
+    nxt = (idx + 1) % 3
+
+    M[idx], M[nxt] = max(M[idx] - (C[nxt] - M[nxt]),
+                         0), min(C[nxt], M[nxt] + M[idx])
+
+for i in M:
+    print(i)
