44f5f096a6c1d0131ddf1100b74f231dcbae8e1c
kimseunggyu <dev.seunggyu@gmail.com>
2020-08-26 22:03:36 +0900

    study array

---
+++ data_structure/1.array.py
@@ -0,0 +1,12 @@
+# 1. 1차원 배열
+data_list = [1, 2, 3, 4, 5]
+print(data_list)
+
+# 2. 2차원 배열
+data_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(data_list)
+
+# 3. 2차원 배열에서 9, 8, 7 출력
+print(data_list[2][2])
+print(data_list[2][1])
+print(data_list[2][0])
