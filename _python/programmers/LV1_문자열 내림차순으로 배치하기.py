d3ee9f0ccd9b1d5e639649da31362e576504af16
kimseunggyu <dev.seunggyu@gmail.com>
2020-09-11 03:51:30 +0900

    algorithm init

--- _python/programmers/LV1_문자열 내림차순으로 배치하기.py
+++
@@ -1,9 +0,0 @@
-def solution(string):
-    string_list = []
-    for char in string:
-        string_list.append(char)
-        
-    string_list.sort()
-    string_list.reverse()
-    
-    return ''.join(string_list)
--- _python/programmers/LV1_자연수 뒤집어 배열로 만들기.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    return [ int(char) for char in str(n)[::-1]]
-
-
-"""
-[참고]
-# def digit_reverse(n):
-    return list(map(int, reversed(str(n))))
-"""
