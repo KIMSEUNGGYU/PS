28a4b3121a6ed8b06b6ab3211e4b53350277e5b5
kimseunggyu <dev.seunggyu@gmail.com>
2020-10-30 15:55:20 +0900

    study dynamic programming

---
+++ algorithm/3.dynamic_programming.py
@@ -0,0 +1,20 @@
+def fibonacci(num):
+    # 피보나치 수열 구하기
+    cache = [0 for i in range(num+1)]
+    cache[0] = 0
+    cache[1] = 1
+
+    for index in range(2, num+1):
+        cache[index] = cache[index-1]+cache[index-2]
+
+    return cache[num]
+
+def test_fibonacci():
+    print("=== fibonacci ===")
+    print("3 fibonacci:", fibonacci(3))
+    print("4 fibonacci:", fibonacci(4))
+    print("10 fibonacci:", fibonacci(10))
+
+
+
+# test_fibonacci()
---
+++ algorithm/test/3.dynamic_programming.py
@@ -0,0 +1,12 @@
+def fibonacci():
+    pass
+
+def test_fibonacci():
+    print("=== fibonacci ===")
+    print("3 fibonacci:", fibonacci(3))
+    print("4 fibonacci:", fibonacci(4))
+    print("10 fibonacci:", fibonacci(10))
+
+
+
+# test_fibonacci()
