f8ff869d60e46efecf5e0312c6e375eca1bb7c0e
kimseunggyu <dev.seunggyu@gmail.com>
2020-11-03 18:26:36 +0900

    study dynamic programming

--- algorithm/3.dynamic_programming.py
+++ algorithm/3.dynamic_programming.py
@@ -1,6 +1,6 @@
 def fibonacci(num):
     # 피보나치 수열 구하기
-    cache = [0 for i in range(num+1)]
+    cache = [0 for i in range(num+1)]   # 캐시
     cache[0] = 0
     cache[1] = 1
 
@@ -14,7 +14,5 @@ def test_fibonacci():
     print("3 fibonacci:", fibonacci(3))
     print("4 fibonacci:", fibonacci(4))
     print("10 fibonacci:", fibonacci(10))
-
-
 
 # test_fibonacci()
