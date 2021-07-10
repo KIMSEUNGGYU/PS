a1803a52ba0edd7c109210de45682262fb4bbc90
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-07-10 15:14:12 +0900

    baekjoon 11004 - 좌표 정렬하기

---
+++ 11650.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/11650
+
+N = int(input())
+
+array = []
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    array.append((x, y))
+
+array.sort()
+
+for x, y in array:
+    print(x, y)
